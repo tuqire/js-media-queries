af17149e634dc8633d7be59f2567cfc8e8463398
Tuqire Hussain <tuqirehussain@tuqires-mbp.home>
2017-09-27 23:19:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# JS Media Queries
+
+Allows the use of media queries in javascript. Caches result on first call, and then updates the value on window resize event.
+
+
+## How it Works
+
+Example use case.
+
+```js
+import setMediaQueries from 'js-media-queries';
+
+// The media queries to register
+// Must have a key registered for 0px
+// Must be numbers only
+const mediaQueries = {
+  xxs: 0,
+  xs: 320,
+  s: 480,
+  m: 768,
+  l: 1024,
+  xl: 1224,
+  xxl: 1824
+};
+
+const getBreakPoint = setMediaQueries(mediaQueries);
+
+// Call getBreakPoint to get the current break point
+// The break point is cached and recalculated when the window is resized
+// will return the key of the breakpoint (xxs, xs, s, m, l, xl)
+const breakPoint = getBreakPoint();
+
+// Example use case
+const screenSize = {
+  xxs: 'small screen mobile',
+  xs: 'medium screen mobile',
+  s: 'large screen mobile',
+  m: 'iPad',
+  l: 'small screen desktop',
+  xl: 'medium screen desktop',
+  xxl: 'large screen desktop'
+};
+
+console.log(`You are currently on a ${screenSize[breakPoint]}`);
+
+```
+
+
+## Contributors
+
+* Tuqire Hussain <me@tuqire.com>
---
+++ src/index.js
@@ -0,0 +1,53 @@
+export default function setMediaQueries(mediaQueries) {
+	const mediaQueriesStore = validateMediaQueries(mediaQueries);
+	let cachedMediaQuery = setMediaQuery(mediaQueriesStore);
+
+	window.addEventListener('resize', () => cachedMediaQuery = setMediaQuery(mediaQueriesStore));
+	return () => cachedMediaQuery;
+}
+
+function validateMediaQueries(mediaQueries) {
+	let zeroMediaQueryFound = false;
+	const defaultMediaQueries = {
+	  xxs: 0,
+	  xs: 320,
+	  s: 480,
+	  m: 768,
+	  l: 1024,
+	  xl: 1224,
+	  xxl: 1824
+	};
+	const defaultMediaQueriesMessage = `Defaulting to: ${JSON.stringify(defaultMediaQueries)}`;
+
+	let errors = !mediaQueries || Object.keys(mediaQueries).some(mediaQuery => {
+		if (typeof mediaQueries[mediaQuery] !== 'number') {
+			console.error('Media queries provided should be integers', defaultMediaQueriesMessage);
+			return true;
+		}
+
+		if (typeof mediaQueries[mediaQuery] < 0) {
+			console.error('Media queries provided should be larger than 0', defaultMediaQueriesMessage);
+			return true;
+		}
+
+		zeroMediaQueryFound = zeroMediaQueryFound || mediaQueries[mediaQuery] === 0;
+		return false;
+	});
+
+	if (mediaQueries && !zeroMediaQueryFound) {
+		console.error('A key should be for 0px break point', defaultMediaQueriesMessage);
+		errors = true;
+	}
+
+	return errors ? defaultMediaQueries : mediaQueries;
+}
+
+function setMediaQuery(mediaQueriesStore) {
+	const mediaQuery = Object.keys(mediaQueriesStore)
+		.sort((mediaQuery1, mediaQuery2) => mediaQueriesStore[mediaQuery1] > mediaQueriesStore[mediaQuery2] ? -1 : 1)
+		.find(mediaQuery => window.matchMedia(`(min-width: ${mediaQueriesStore[mediaQuery]}px)`).matches);
+
+		console.log('computing...', mediaQuery);
+
+	return mediaQuery;
+}
